6f0b7359ca86387e10c48bc4bf461fb2a888f46d
unknown <thiep.wong@gmail.com>
2019-10-09 00:46:35 +0700

    Cap nhat

--- services/auth/model.go
+++ services/auth/model.go
@@ -42,7 +42,7 @@ type SignInModel struct {
 	Username string `json:"username" bson:"username"`
 	Password string `json:"password" bson:"password"`
 	System   string `json:"system" bson:"system"`
-	Expired  int64  `json:"expired"`
+	Expired  int    `json:"expired"`
 }
 
 type ActivateModel struct {
